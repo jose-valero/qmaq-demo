065df39aa1aef7695592ee764d50d00493d0cbaa
Jose Valero <josemiguelvaleroreyes@gmail.com>
2020-07-04 19:52:21 -0300

    fix del index

--- src/views/Home/Index.vue
+++ src/views/Home/Index.vue
@@ -2,21 +2,22 @@
   <div class="container my-2 bg-warning">
     <h3 class="text-primary">Busqueda</h3>
     <div class="row my-2">
-      <div class="col-6">
+      <div class="col-6 ">
         <calendar rango="Desde: " />
         <DDCategoria />
       </div>
-      <div class="col-6">
+      <div class="col-6 ">
         <calendar rango="Hasta: " />
         <DDMaquinaria />
       </div>
-      <!-- <TxTotales class="mt-3" />
-      <TxShareOfSegment />-->
+      <TxTotales class="mt-3" />
+      <TxShareOfSegment />
     </div>
     <div class="submit">
-      <div class="row mx-3 py-3">
-        <Button label="Buscar" class="p-button-raised p-button-secondary" />
-      </div>
+    <div class="row mx-3 py-3 ">
+      <Button label="Buscar" class="p-button-raised p-button-secondary" />
+    </div>
+
     </div>
   </div>
 </template>
@@ -26,22 +27,23 @@ import Calendar from "./Calendar";
 
 import DDMaquinaria from "./DDMaquinaria";
 import DDCategoria from "./DDCategoria";
-// import TxTotales from "../Tables/Hidrogruas/TxTotales";
-// import TxShareOfSegment from "../Tables/Hidrogruas/TxShareOfSegment";
+import TxTotales from "../Tables/Hidrogruas/TxTotales";
+import TxShareOfSegment from "../Tables/Hidrogruas/TxShareOfSegment";
 
 export default {
   name: "Home",
   components: {
     Calendar,
     DDMaquinaria,
-    DDCategoria
-    // TxTotales,
-    // TxShareOfSegment
+    DDCategoria,
+    TxTotales,
+    TxShareOfSegment
   }
 };
 </script>
 
 <style lang="stylus">
-
+ .submit 
+  text-align: right
 </style>
 
