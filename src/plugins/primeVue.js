3cfc21abfb05b84ff8fb8fdf898adb1386c21c16
celeste lopez <celeste2325@gmail.com>
2020-07-13 17:45:50 -0300

    apocalipsis

--- src/plugins/primeVue.js
+++ src/plugins/primeVue.js
@@ -4,12 +4,14 @@ import Calendar from "primevue/calendar";
 import MultiSelect from 'primevue/multiselect';
 import Dropdown from 'primevue/dropdown';
 import Button from 'primevue/button';
-
+import FileUpload from 'primevue/fileupload';
 import "primevue/resources/themes/vela-orange/theme.css"       //theme
 import "primevue/resources/primevue.min.css"                 //core css
-import "primeicons/primeicons.css"
+import "primeicons/primeicons.css";
+import 'primeflex/primeflex.css';
 
 Vue.component("Calendar", Calendar);
 Vue.component("MultiSelect", MultiSelect);
 Vue.component("Dropdown", Dropdown);
 Vue.component("Button", Button);
+Vue.component("FileUpload", FileUpload);
